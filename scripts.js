e3742205a724371c06ab7e20fc4717efd2069766
Bruno Oliveira <brunoroliveira7@gmail.com>
2024-10-09 11:27:33 -0300

    initial commit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const guests = document.querySelector("ul");
+
+const newGuest = document.createElement("li");
+newGuest.classList.add("guest");
+
+const guestName = document.createElement("span");
+
+guestName.textContent = "Bruno";
+
+
+newGuest.append(guestName);
+
+guests.append(newGuest);
+
+
